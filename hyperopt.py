0e2b265443ed8810537005757749512b8030b120
preissner <simon.preissner@gmx.de>
2019-02-22 14:18:14 +0100

    refactored parameter input in hyperopt.py to methods

--- hyperopt.py
+++ hyperopt.py
@@ -25,87 +25,62 @@ if len(sys.argv) < 2 or sys.argv[1] not in ["bnc","wiki","rawiki","w2v","5k"]:
     sys.exit() 
 
 
-#========== PARAMETER INPUT
-if sys.argv[1] == "bnc":
-    data = "data/BNC-MEN.dm"
-    column_labels = "data/BNC-MEN.cols"
-    MEN_annot = "data/MEN_dataset_lemma_form_full"
-elif sys.argv[1] == "wiki":
-    data = "data/wiki_all.dm"
-    column_labels = "data/wiki_all.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
-elif sys.argv[1] == "rawiki":
-    data = "data/wiki_abs-freq.dm"
-    column_labels = "data/wiki_abs-freq.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
-elif sys.argv[1] == "w2v":
-    data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_100m_w2v_400.txt"
-    column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac.w2v.400.vocab"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
-elif sys.argv[1] == "5k":
-    data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k.dm"
-    column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
-
-if "-logto" in sys.argv: 
-    log_dest = sys.argv[sys.argv.index("-logto")+1]
-else: 
-    log_dest = "./log/hyperopt/default_log"
-if not os.path.isdir(log_dest):
-   os.makedirs(log_dest)
-
-if "-kc" in sys.argv:
-    kc_factor_min = int(sys.argv[sys.argv.index("-kc")+1]) 
-    kc_factor_max = int(sys.argv[sys.argv.index("-kc")+2]) 
-    kc_steps = int(sys.argv[sys.argv.index("-kc")+3]) 
-else:
-    kc_factor_min = 4 # number of Kenyon cells (in steps of 4) = 5params
-    kc_factor_max = 20
-    kc_steps = 4
-if "-proj" in sys.argv:
-    projections_min = int(sys.argv[sys.argv.index("-proj")+1]) 
-    projections_max = int(sys.argv[sys.argv.index("-proj")+2]) 
-    proj_steps = int(sys.argv[sys.argv.index("-proj")+3]) 
-else:
-    projections_min = 4 # number of connections to any given KC (in steps of 4) = 5params
-    projections_max = 20
-    proj_steps = 4
-if "-hash" in sys.argv:
-    hash_perc_min = int(sys.argv[sys.argv.index("-hash")+1]) 
-    hash_perc_max = int(sys.argv[sys.argv.index("-hash")+2]) 
-    hash_steps = int(sys.argv[sys.argv.index("-hash")+3]) 
-else:
-    hash_perc_min = 2 # percent of winners for the final hash (e.g. 2-10 in steps of 2) = 5params
-    hash_perc_max = 10
-    hash_steps = 2
-
-flattening = []
-if "log" in sys.argv:
-    flattening.append("log")
-if "log2" in sys.argv:
-    flattening.append("log2")
-if "log2" in sys.argv:
-    flattening.append("log10")
-if not flattening:
-    flattening = ["log", "log2", "log10"] # flattening happens before the PN layer (ln, log2, log10) = 3params
-
-if "-v" in sys.argv:
-    verbose = True
-else:
-    verbose = False
-
-
-in_space = utils.readDM(data) # returns dict of word : word_vector
-i_to_cols, cols_to_i = utils.readCols(column_labels) # returns both-ways dicts of the vocabulary (word:pos_in_dict); important for maintenances
-pn_size = len(i_to_cols) # length of word vector (= input dimension)
-
-all_ff_specs = {}
-internal_log = {}
-sp_diffs = {}
-
-
-
 #========== FUNCTIONS
+def get_text_resources_from_argv():
+    if sys.argv[1] == "bnc":
+        data = "data/BNC-MEN.dm"
+        column_labels = "data/BNC-MEN.cols"
+        MEN_annot = "data/MEN_dataset_lemma_form_full"
+    elif sys.argv[1] == "wiki":
+        data = "data/wiki_all.dm"
+        column_labels = "data/wiki_all.cols"
+        MEN_annot = "data/MEN_dataset_natural_form_full"
+    elif sys.argv[1] == "rawiki":
+        data = "data/wiki_abs-freq.dm"
+        column_labels = "data/wiki_abs-freq.cols"
+        MEN_annot = "data/MEN_dataset_natural_form_full"
+    elif sys.argv[1] == "w2v":
+        data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_100m_w2v_400.txt"
+        column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac.w2v.400.vocab"
+        MEN_annot = "data/MEN_dataset_natural_form_full"
+    elif sys.argv[1] == "5k":
+        data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k.dm"
+        column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k.cols"
+        MEN_annot = "data/MEN_dataset_natural_form_full"
+    else: 
+        print("Error reading files. Is the key word correct?")
+        sys.exit()
+    return data, column_labels, MEN_annot
+
+def get_ranges_from_argv(param, minimum=4, maximum=20, steps=4):
+    if param in sys.argv:
+        minimum = int(sys.argv[sys.argv.index(param)+1]) 
+        maximum = int(sys.argv[sys.argv.index(param)+2]) 
+        steps = int(sys.argv[sys.argv.index(param)+3])
+    return minimum, maximum, steps 
+
+def get_flattening_from_argv():
+    flattening = []
+    if "log" in sys.argv:
+        flattening.append("log")
+    if "log2" in sys.argv:
+        flattening.append("log2")
+    if "log10" in sys.argv:
+        flattening.append("log10")
+    if not flattening:
+        flattening = ["log", "log2", "log10"] # flattening happens before the PN layer (ln, log2, log10) = 3params
+    return flattening
+
+def get_logging_from_argv():
+    if "-logto" in sys.argv: 
+        log_dest = sys.argv[sys.argv.index("-logto")+1]
+    else: 
+        log_dest = "./log/hyperopt/default_log"
+    if not os.path.isdir(log_dest):
+        os.makedirs(log_dest)
+    return log_dest
+
+
 
 def evaluate(orig_space, result_space, goldstd):
     sp_before = 0
@@ -117,7 +92,6 @@ def evaluate(orig_space, result_space, goldstd):
     results = {"testset":count_after, "sp_before":sp_before, "sp_after":sp_after, "sp_diff":sp_diff}
     return results
 
-
 def log_results(results, flattening, ff_config, log_dest, result_space=None, pair_cos=True):
     pns = ff_config["pn_size"]
     kcs = ff_config["kc_size"]
@@ -126,7 +100,6 @@ def log_results(results, flattening, ff_config, log_dest, result_space=None, pai
     
     logfilepath = log_dest+"/"+sys.argv[1]+"-"+str(int(kcs/pns))+"-"\
                   +str(proj)+"-"+str(int((hp*kcs)/100))+"-"+flattening+".txt"
-
 
     items = results["testset"]
     spb = round(results["sp_before"], 5)
@@ -158,7 +131,26 @@ def log_results(results, flattening, ff_config, log_dest, result_space=None, pai
         print(results_statement, "\n")
 
 
+#========== PARAMETER INPUT
 
+data, column_labels, MEN_annot = get_text_resources_from_argv()
+log_dest = get_logging_from_argv()
+
+flattening = get_flattening_from_argv()
+
+kc_factor_min, kc_factor_max, kc_steps = get_ranges_from_argv("-kc")
+projections_min, projections_max, proj_steps = get_ranges_from_argv("-proj")
+hash_perc_min, hash_perc_max, hash_steps = get_ranges_from_argv("-hash", minimum=2, maximum=10, steps=2)
+
+in_space = utils.readDM(data) # returns dict of word : word_vector
+i_to_cols, cols_to_i = utils.readCols(column_labels) # returns both-ways dicts of the vocabulary (word:pos_in_dict); important for maintenances
+pn_size = len(i_to_cols) # length of word vector (= input dimension)
+
+# for reporting purposes
+verbose = "-v" in sys.argv
+all_ff_specs = {}
+internal_log = {}
+sp_diffs = {}
 
 
 #========== GRID SEARCH
