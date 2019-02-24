c9fd1f2381b940100d4bb467ca7f66c272e13190
preissner <simon.preissner@gmx.de>
2019-02-24 23:10:57 +0100

    ...

--- projection.py
+++ projection.py
@@ -45,7 +45,8 @@ elif sys.argv[1] == "1k":
 elif sys.argv[1] == "5k":
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_MEN-checked.dm"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_MEN-checked.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
+    #MEN_annot = "data/MEN_dataset_natural_form_full"
+    MEN_annot = "data/MEN_dataset_lemma_form_full"
 elif sys.argv[1] == "10k":
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_MEN-checked.dm"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_MEN-checked.cols"
