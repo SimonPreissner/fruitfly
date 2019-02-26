816c1b8c5658ebeca87f8b8a95d61f8d39b9531b
preissner <simon.preissner@gmx.de>
2019-02-26 12:01:21 +0100

    ...

--- checker.py
+++ checker.py
@@ -1,4 +1,3 @@
-import re
 import numpy as np
 
 """
@@ -8,42 +7,35 @@ is in the ukwac_100m corpus.
 
 
 wacfile = "../ukwac_100m/ukwac_100m_oneline.txt"
-#wacfile = "data/ukwac_100m_oneline_demo.txt"
 menfile = "data/MEN_dataset_natural_form_full"
-#menfile = "data/BNC-MEN.cols"
 
 wordlist_WAC = []
 checklist = []
 unshared_words = []
 
-reg = re.compile("_.+?")
-
-
 with open(wacfile, "r") as f:
+    print("reading",wacfile,"...")
     for line in f:
         line = line.rstrip() 
         wordlist_WAC.extend(line.split())
-        print("items already read:",len(wordlist_WAC))
+    print("Finished reading. Tokens in the corpus:",len(wordlist_WAC))
             
 with open(menfile, "r") as f:
-    if menfile == "data/MEN_dataset_natural_form_full": 
-        for line in f:
-            words = line.rstrip().split()[:2]
-            checklist.extend(words)
-    else:
-        for word in f:
-            word = word.rstrip()
-            word = re.sub(reg, "",word)
-            checklist.appent(word)
+    for line in f:
+        words = line.rstrip().split()[:2]
+        checklist.extend(words)
 
-unshared_words = list(set(checklist).difference(set(wordlist_WAC)))
+wordset_WAC = set(wordlist_WAC)
+checkset = set(checklist)
+print("Types in the corpus:   ", len(wordset_WAC))
+print("Types in the checklist:", len(checkset))
 
-if unshared_words is True:
-    print("all required words are in the corpus.")
+unshared_words = list(checkset.difference(wordset_WAC))
+
+if unshared_words is False:
+    print("All required words are in the corpus.")
 else:
     print(unshared_words[:min(int(np.ceil(len(unshared_words)/10)), 25)])
     print("These are some of the",len(unshared_words),"words are not in the corpus.")
 
-print("types in the corpus:   ", len(set(wordlist_WAC)))
-print("types in the checklist:", len(set(checklist)))
             
