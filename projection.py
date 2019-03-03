86eddccbffb1a8d43cb5ce97a32a642e2a3c9bfd
preissner <simon.preissner@gmx.de>
2019-03-03 21:23:44 +0100

    added gold standard options to projection.py

--- projection.py
+++ projection.py
@@ -13,8 +13,10 @@ This script reads command line options and feeds data to a Fruitfly object.
 
 #=============== PARAMETER INPUT
 
-if len(sys.argv) < 5 or sys.argv[1] not in ("bnc","wiki","rawiki","w2v","1k","5k","10k"):
-    print("\nUSAGE: python3 projection.py bnc|wiki|rawiki|w2v|1k|5k|10k [num-kc] [size-proj] [percent-hash]\n\
+if len(sys.argv) < 6 or sys.argv[1] not in ("bnc","wiki","rawiki","w2v","1k","5k","10k"):
+    print("\nUSAGE: python3 projection.py [space] [goldstd] [num-kc] [size-proj] [percent-hash]\n\
+    - space: bnc|wiki|rawiki|w2v|1k|5k|10k\n\
+    - goldstd: men|lemma|sand\n\
     - num-kc: the number of Kenyon cells\n\
     - size-proj: how many projection neurons are used for each projection\n\
     - percent-hash: how much of the Kenyon layer to keep in the final hash.\n\
@@ -25,45 +27,52 @@ if len(sys.argv) < 5 or sys.argv[1] not in ("bnc","wiki","rawiki","w2v","1k","5k
 if sys.argv[1] == "bnc":
     data = "data/BNC-MEN.dm"
     column_labels = "data/BNC-MEN.cols"
-    MEN_annot = "data/MEN_dataset_lemma_form_full"
 elif sys.argv[1] == "wiki":
     data = "data/wiki_all.dm"
     column_labels = "data/wiki_all.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
 elif sys.argv[1] == "rawiki":
     data = "data/wiki_abs-freq.dm"
     column_labels = "data/wiki_abs-freq.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
 elif sys.argv[1] == "w2v":
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_100m_w2v_400.txt"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac.w2v.400.vocab"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
 elif sys.argv[1] == "1k":
     #data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_1k_MEN-checked.dm"
     #column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_1k_MEN-checked.cols"
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_1k_GS-checked.dm"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_1k_GS-checked.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
 elif sys.argv[1] == "5k":
     #data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_MEN-checked.dm"
     #column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_MEN-checked.cols"
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_GS-checked.dm"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_5k_GS-checked.cols"
-    MEN_annot = "data/MEN_dataset_natural_form_full"
 elif sys.argv[1] == "10k":
     #data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_MEN-checked.dm"
     #column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_MEN-checked.cols"
     data = "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_GS-checked.dm"
     column_labels= "/home/simon.preissner/FFP/ukwac_100m/ukwac_10k_GS-checked.cols"
+else: 
+    print("No data assignable; check the [space] parameter!")
+    sys.exit()
+
+if sys.argv[2] == "men":
     MEN_annot = "data/MEN_dataset_natural_form_full"
+elif sys.argv[2] == "lemma":
+    MEN_annot = "data/MEN_dataset_lemma_form_full"
+elif sys.argv[2] == "sand":
+    MEN_annot = "incrementality_sandbox/data/sandbox_MEN_pairs"
+else:
+    print("No gold standard assignable; check the [goldstd] parameter!")
+    sys.exit()
+
 
 unhashed_space = utils.readDM(data) # returns dict of word : word_vector
 i_to_cols, cols_to_i = utils.readCols(column_labels) # returns both-ways dicts of the vocabulary (word:pos_in_dict); important for maintenances
 
 pn_size = len(cols_to_i) # length of word vector (= input dimension)
-kc_size = int(sys.argv[2])
-proj_size = int(sys.argv[3]) # number of connections to any given KC
-hash_percent = int(sys.argv[4])
+kc_size = int(sys.argv[3])
+proj_size = int(sys.argv[4]) # number of connections to any given KC
+hash_percent = int(sys.argv[5])
 #print("SIZES PN LAYER:",pn_size,"KC LAYER:",kc_size)
 #print("SIZE OF PROJECTIONS:",proj_size)
 #print("SIZE OF FINAL HASH:",hash_percent,"%")
