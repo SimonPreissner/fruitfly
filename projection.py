825153f7be3f85b6d7f70fece656d272bcff83c3
preissner <simon.preissner@gmx.de>
2019-03-13 18:44:13 +0100

    more work on iterations for projection.py

--- projection.py
+++ projection.py
@@ -99,7 +99,7 @@ if iterations > 1:
     all_spd_norm = [e+best[-1] for e in best]
 
     print("\nFinished all",iterations,"runs. Summary:")
-    print("best and worst runs:",[round(e, 4) for e in best[:3]]+[round(e, 4) for e in best[:-3]])
+    print("best and worst runs:",[round(e, 4) for e in best[:3]].append([round(e, 4) for e in best[:-3]]))
     print("mean Sp. before:    ",round(np.mean(all_spb), 4))
     print("mean Sp. after:     ",round(np.mean(all_spa), 4))
     print("mean Sp. difference:",round(np.mean(all_spd), 4))
