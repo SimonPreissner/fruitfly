90e30636ded548d6b80ac3d42e4e32f46f6bf214
preissner <simon.preissner@gmx.de>
2019-03-13 18:35:32 +0100

    more work on iterations for projection.py

--- projection.py
+++ projection.py
@@ -58,6 +58,8 @@ all_spb = []
 all_spa = []
 all_spd = []
 for i in range(iterations):
+    if iterations > 1:
+        print("\n#=== NEW RUN:",i+1,"===#")
     #=============== FOR PURE EVALUATION OF UNHASHED SPACES
 
     if evaluate_mode:
@@ -88,13 +90,25 @@ for i in range(iterations):
     all_spa.append(spa)
     all_spd.append(spa-spb)
 if iterations > 1:
+    best_before = sorted(all_spb, reverse=True)
+    best_after = sorted(all_spa, reverse=True)
+    best = sorted(all_spd, reverse=True)
+
+    all_spb_norm = [e+best_before[-1] for e in best_before]
+    all_spa_norm = [e+best_after[-1] for e in best_after]
+    all_spd_norm = [e+best[-1] for e in best]
+
     print("\nFinished all",iterations,"runs. Summary:")
+    print("best and worst runs:",[round(e, 4) for e in best[:3]]+[round(e, 4) for e in best[:-3]])
     print("mean Sp. before:    ",round(np.mean(all_spb), 4))
     print("mean Sp. after:     ",round(np.mean(all_spa), 4))
     print("mean Sp. difference:",round(np.mean(all_spd), 4))
-    print("variance of Sp. before:    ",round(np.var(all_spb),8))
-    print("variance of Sp. after:     ",round(np.var(all_spa),8))
-    print("variance of Sp. difference:",round(np.var(all_spd),8))
+    print("var of Sp. before:    ",round(np.var(all_spb_norm),8))
+    print("var of Sp. after:     ",round(np.var(all_spa_norm),8))
+    print("var of Sp. difference:",round(np.var(all_spd_norm),8))
+    print("std of Sp. before:     ",round(np.std(all_spb_norm), 8))
+    print("std of Sp. after:      ",round(np.std(all_spa_norm), 8))
+    print("std of Sp. difference: ",round(np.std(all_spd_norm), 8))
 
 #========== PARAMETER VALUES
 """
