5698ab4ffb5031d9ba95357a3c9663f62dd56434
SimonPreissner <simon.preissner@gmx.de>
2019-05-29 15:40:32 +0200

    added resources for walkthrough code

--- corpuschunker.py
+++ corpuschunker.py
@@ -1,15 +1,19 @@
 import math
 
-corpus = "data/pride.txt" # alternative: sandbox_corpus_multiline.txt
+corpus = "../ukwac_100m/ukwac_100m.txt" # alternative: sandbox_corpus_multiline.txt
 outdir = "data/chunks/"
-w_per_file = 100000
+w_per_file = 1000000 # 1m
+w_limit = 10000000 # 10m
 
 wordlist = []
 with open(corpus, "r") as f:
     print("reading from",corpus,"...")
     for line in f:
         wordlist.extend(line.rstrip().split())
-        print("word count:",len(wordlist))
+        print("words read:",len(wordlist), end="\r")
+        if len(wordlist) >= w_limit:
+            break
+
 
 for wc in range (0, len(wordlist), w_per_file):
     nr= math.ceil(wc/w_per_file)
