f3673c74215dd0c77908c06f8f9b2adfd0e33664
SimonPreissner <simon.preissner@gmx.de>
2019-06-24 10:49:08 +0200

    changed input

--- simple_corpus_tagger.py
+++ simple_corpus_tagger.py
@@ -10,7 +10,7 @@ postags = {"N" :["NN", "NNS", "NNP", "NNPS"],
            "V" :["VB", "VBD", "VBG", "VBN", "VBZ", "VBP"],
            "J" :["JJ", "JJR", "JJS"]}
 
-corpusfile =    "/mnt/8tera/shareclic/fruitfly/ukwac_100m.txt" #"data/potato.txt" #CLEANUP
+corpusfile =    "../ukwac_100m/ukwac_100m_oneline.txt" #"data/potato.txt" #CLEANUP
 tagged_corpus = "/mnt/8tera/shareclic/fruitfly/ukwac_100m_tok-tagged.txt" #"test/postag_potato.txt" #CLEANUP
 
 def simplify_postags(tagged_words):
