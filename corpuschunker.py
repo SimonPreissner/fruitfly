66853857b1edd1556de05c14ead40dc9a0f0494e
preissner <simon.preissner@gmx.de>
2019-09-09 13:49:07 +0200

    prepared corpuschunker for wiki chunking

--- corpuschunker.py
+++ corpuschunker.py
@@ -1,9 +1,13 @@
 import math
+import os
 
 corpus = "/mnt/8tera/corpora/enwiki-20181120/enwiki-20181120-pages-meta-current10.txt-p2336425p3046511"
-outdir = "data/chunks_wiki/"
+outdir = "./data/chunks_wiki/"
 w_per_file = 100000 # 1m
 w_limit = 1000000 # 10m
+
+if not os.path.isdir(outdir):
+    os.makedirs(outdir, exist_ok=True)
 
 wordlist = []
 with open(corpus, "r") as f:
@@ -30,7 +34,7 @@ corpus =
 "/mnt/8tera/corpora/enwiki-20181120/enwiki-20181120-pages-meta-current10.txt-p2336425p3046511"
 
 outdir = 
-"data/chunks/"
-"data/chunks_wiki/"
+"./data/chunks/"
+"./data/chunks_wiki/"
 
 """
